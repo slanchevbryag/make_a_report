4170513fde41242c79d9b1b735382f0a46273627
Slanchevbryag <slanchevbryag@yandex.ru>
2024-10-04 14:26:52 +0300

    Вывод параметров погоды. Без проверок

---
+++ coordinates.py
@@ -0,0 +1,17 @@
+﻿import re
+
+def getting_coordinats(filepath_track): #эта функция получает координаты начальной и конечной точки пути
+          
+    listpoint = []
+
+    with open(filepath_track, 'r', encoding='utf-8') as f: #открываем файл .gpx
+        for line in f:
+            if "<trkpt" in line:                           #и берём из него только строки с координатами точек трека
+                listpoint.append(line)
+
+    first_point = re.findall(r'\d\d.\d\d\d\d\d\d', listpoint[0])  #нам нужна первая и последняя строки 
+    last_point = re.findall(r'\d\d.\d\d\d\d\d\d', listpoint[-1])  #это кординаты начала и конца трека 
+    return first_point, last_point
+
+if __name__=='__main__':
+    print(getting_coordinats('c:\\projects\\make_a_report\\tracks\\1_day.gpx'))
