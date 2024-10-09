46fda1cdef8fe9fa536464d1cf3190d250f56828
slanchevbryag <slanchevbryag@yandex.ru>
2024-10-09 23:13:38 +0300

    Отформатировано по PEP8

--- survey.py
+++ survey.py
@@ -2,7 +2,7 @@
 import weather
 
 date = input("Укажите дату в формате yyyy-MM-dd: ")
-filepath_track = input("Укажите путь к файлу gtx: ")
+filepath_track = input("Укажите путь к файлу gpx: ")
 
 point = coordinates.getting_coordinats(filepath_track)
 point_coordinates = point[0]
@@ -11,6 +11,8 @@ point_coordinates = " ".join(point_coordinates)
 weather_and_astro = weather.weather_by_terrain(point_coordinates, date)
 
 print(f"Дата: {date}")
-print(f"Восход: {weather_and_astro['sunrise']} Закат: {weather_and_astro['sunset']}")
-print(f"Max температура: {weather_and_astro['maxtempC']} С Min температура: {weather_and_astro['mintempC']} С")
+print(f"Восход: {weather_and_astro['sunrise']}")
+print(f"Закат: {weather_and_astro['sunset']}")
+print(f"Max температура: {weather_and_astro['maxtempC']} С")
+print(f"Min температура: {weather_and_astro['mintempC']} С")
 print(weather_and_astro["weatherDesc"])
